68c3534f8b696dba4c4d7a24486f61d67f8c3fc3
Rosmel_OTIS <rosmel097@gmail.com>
2024-05-30 12:41:44 -0500

    Change form template registerdoc, updatedocd

--- docscan/forms.py
+++ docscan/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Document, Origin
+from .models import Document
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
 
 
 class DocumentForm(ModelForm):
@@ -8,13 +10,33 @@ class DocumentForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'card card-body mb-5'
+        self.helper.layout = Layout(
+            Row(
+                Column('dateregister', css_class='form-group col-md-6 mb-0'),
+                Column('doctype', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('description', css_class='form-group col-md-6 mb-0'),
+                Column('direction', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('origin', css_class='form-group col-md-6 mb-0'),
+                Column('folios', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            'fileupload',
+            Submit('submit', 'Enviar', css_class='btn btn-primary')
+        )
+
         self.fields['doctype'].empty_label = 'Escoge documento ...'
         self.fields['direction'].empty_label = 'Escoge dirección ...'
-
         self.fields['origin'].empty_label = 'Escoge origen ...'
     
-
-
     class Meta:     
         model = Document
         fields = ['dateregister', 'doctype','direction', 'origin', 'description', 'folios' , 'fileupload',]
--- docscan/static/js/jsuser.js
+++ docscan/static/js/jsuser.js
@@ -2,20 +2,7 @@
 const $txta = document.querySelector('textarea.textarea')
 if ($txta) $txta.rows = 2
 
-// Borrar asteriscos de (requerdio) del formulario RegisterDoc
-const $asteriskAll = document.querySelectorAll('.asteriskField')
-for (let i = 0; i < $asteriskAll.length; i++) {
-    $asteriskAll[i].innerHTML = ''
-}
-
-// Mover boton Registrar dentro del formulario
-const $formRegisterDoc=document.querySelector('.form-registerdoc'),
-$btnRegisterDoc=document.querySelector('.btn-registerdoc');
-if($btnRegisterDoc) $formRegisterDoc.append($btnRegisterDoc)
-
-// Validar tamaño de PDF
 const MAX_FILE_SIZE = 30000000; // 1MB = 1 millón de bytes
-
 const $fileInput = document.querySelector("#id_fileupload");
 
 if($fileInput) {
